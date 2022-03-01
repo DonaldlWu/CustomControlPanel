35d502ed8483191e09bbf5c8d63bd821882e841c
Dei Ren Wu <deirenwu1101@gmail.com>
2022-03-01 18:14:34 +0800

    Initial Commit

---
+++ Shared/CustomControlPanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomControlPanelApp.swift
+//  Shared
+//
+//  Created by 吳得人 on 2022/3/1.
+//
+
+import SwiftUI
+
+@main
+struct CustomControlPanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
